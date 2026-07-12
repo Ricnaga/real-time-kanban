55d2270f89ac121e33277a84b77bf09797a033f6
ricnaga <ric.nagato@gmail.com>
2026-07-11 21:09:13 -0300

    refactor: renomeia export db para drizzleDB no drizzle.database

--- apps/server/src/backend/shared/infra/database/drizzle/drizzle.database.ts
+++ apps/server/src/backend/shared/infra/database/drizzle/drizzle.database.ts
@@ -4,4 +4,4 @@ import * as schema from './drizzle.schema'
 
 const env = loadEnvironment()
 
-export const db = drizzle(env.DATABASE_URL, { schema })
+export const drizzleDB = drizzle(env.DATABASE_URL, { schema })
