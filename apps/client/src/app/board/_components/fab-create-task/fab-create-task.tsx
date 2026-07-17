9cf5d8f5bd5db9ecee1b95ff88f979aa01a8894a
ricnaga <ric.nagato@gmail.com>
2026-07-17 16:51:51 -0300

    feat: substituir SVG inline por PlusIcon no FAB
    Troca o SVG manual do botao flutuante pelo PlusIcon do heroicons.

--- apps/client/src/app/board/_components/fab-create-task/fab-create-task.tsx
+++ apps/client/src/app/board/_components/fab-create-task/fab-create-task.tsx
@@ -1,3 +1,4 @@
+import { PlusIcon } from '@heroicons/react/24/outline';
 import { fabStyles } from './fab-create-task.tv';
 
 type FabCreateTaskProps = {
@@ -12,20 +13,7 @@ export function FabCreateTask({ onClick }: FabCreateTaskProps) {
       aria-label="Criar task"
       className={fabStyles()}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M12 5v14" />
-        <path d="M5 12h14" />
-      </svg>
+      <PlusIcon className="size-6" />
     </button>
   );
 }
